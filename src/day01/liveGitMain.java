b6c89e55ccc897abfb223b0f77b7e97c40cdb57e
hamed <hamedjaberi27@gmail.com>
2026-03-22 12:58:44 -0400

    helloWorld class updated

--- src/day01/liveGitMain.java
+++ src/day01/liveGitMain.java
@@ -3,5 +3,6 @@ package day01;
 public class liveGitMain {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("first change : practicing github");
     }
 }
